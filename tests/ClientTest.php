ce1492bd7d451f2c38752b59591438832f81e51a
jacobemerick <jpemeric@gmail.com>
2016-01-08 15:09:30 -0700

    Adds a more stringent test for the general request method

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -52,33 +52,42 @@ class ClientTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($expectedImageResource, $imageResource);
     }
 
-    public function testRequest()
+    /**
+     * @dataProvider dataRequests
+     */
+    public function testRequest($method, $uri, $options, Response $response, $compiledUri)
     {
-        $responseBody = json_encode(['key' => 'value']);
-        $expectedResponse = new Response(200, [], $responseBody);
-
-        $client = $this->newClientWithMockHandler([$expectedResponse]);
-        $response = $client->request('GET', '/', []);
-
-        $this->assertSame($expectedResponse, $response);
-    }
-
-    protected function newClient()
-    {
-        return new Client('client_id', 'client_secret');
-    }
-
-    protected function newClientWithMockHandler(array $responseQueue)
-    {
-        $mock = new MockHandler($responseQueue);
-        $handler = HandlerStack::create($mock);
+        $mockHandler = new MockHandler([$response]);
         $client = $this->newClient();
 
         $reflectedClient = new ReflectionClass($client);
         $reflectedProperty = $reflectedClient->getProperty('guzzle');
         $reflectedProperty->setAccessible(true);
+        $reflectedProperty->setValue($client, new Guzzle(['handler' => HandlerStack::create($mockHandler)]));
 
-        $reflectedProperty->setValue($client, new Guzzle(['handler' => $handler]));
-        return $client;
+        $testResponse = $client->request($method, $uri, $options);
+        $lastRequest = $mockHandler->getLastRequest();
+
+        $this->assertSame($response, $testResponse);
+        $this->assertEquals($method, $lastRequest->getMethod());
+        $this->assertEquals($compiledUri, $lastRequest->getUri());
+    }
+
+    public function dataRequests()
+    {
+        return [
+            [
+                'method' => 'GET',
+                'uri' => '/test',
+                'options' => ['query' => ['key' => 'value']],
+                'response' => new Response(200, [], json_encode(['key' => 'value'])),
+                'compiled_uri' => '/test?key=value',
+            ],
+        ];
+    }
+
+    protected function newClient()
+    {
+        return new Client('client_id', 'client_secret');
     }
 }
