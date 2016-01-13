1b55d7d69d4c8e869754633d37be19f6ca93cd3f
jacobemerick <jpemeric@gmail.com>
2016-01-13 09:32:09 -0700

    Adds detection for empty strings to avoid PHP7 errors

--- src/JsonStream.php
+++ src/JsonStream.php
@@ -15,8 +15,12 @@ class JsonStream implements StreamInterface, JsonSerializable
     public function jsonSerialize()
     {
         $contents = (string) $this->getContents();
+        if ($contents === '') {
+            return null;
+        }
+
         $decodedContents = json_decode($contents, true);
-        if (json_last_error() != JSON_ERROR_NONE) {
+        if (json_last_error() !== JSON_ERROR_NONE) {
             throw new RuntimeException('Error trying to decode response: ' . json_last_error_msg());
         }
 
